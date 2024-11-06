a575eba0dff4add65d7d6af5e9bf2e0fee4493d6
Manuel Garfia <manuelgarfiacastro@gmail.com>
2024-11-06 19:33:30 +0100

    ejercicio 5

--- Tema3/src/boletin1/Ejercicio5.java
+++ Tema3/src/boletin1/Ejercicio5.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// Creo una tabla para los 10 números a introducir
-		double numeros[] = new double[10];
+
 		// Creao un escaner para que el usuario pueda introducir los números
 		Scanner teclado = new Scanner(System.in);
-		double sumaNumeros;
-		double max = Double.MAX_VALUE;
-		double min = Double.MIN_VALUE;
+
+		// Creo una tabla para los 10 números a introducir
+		double numeros[] = new double[10];
+		double sumaNumeros = 0;
+		double max = Double.MIN_VALUE;
+		double min = Double.MAX_VALUE;
+
 		// Creo un for para que el usuario pueda modificar la tabla
 		for (int i = 0; i < numeros.length; i++) {
 			System.out.println("Introduce un número: ");
 			numeros[i] = teclado.nextDouble();
+			sumaNumeros += numeros[i];
+			// Creo estos if para poder sacar el número máximo y el número mínimo
+			if (numeros[i] > max) {
+				max = numeros[i];
+			}
+			if (numeros[i] < min) {
+				min = numeros[i];
+			}
 		}
-		// Creo un for-each para proyectar la tabla
-		for (double valor : numeros) {
-			System.out.print(valor + " ");
-		}
+		// Mostrar suma, máximo y mínimo
+		System.out.println("La suma de los números es: " + sumaNumeros);
+		System.out.println("El número mayor es: " + max);
+		System.out.println("El número menor es: " + min);
 
 	}
 
