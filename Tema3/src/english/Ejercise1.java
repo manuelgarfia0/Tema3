40a7fdded1a6717216e9d175d929324377095c7b
manuelgarfia0 <manuel.garfia@iesnervion.es>
2024-11-11 12:52:56 +0100

    Ejercicios ingles

---
+++ Tema3/src/english/Ejercise1.java
@@ -0,0 +1,42 @@
+package english;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercise1 {
+
+	public static void main(String[] args) {
+		// We create a scanner to let the user introduce the numbers of the array
+		Scanner keyboard = new Scanner(System.in);
+		// We create the array to store the numbers
+		int numbers[] = new int[10];
+		// We create a variable named value
+		int value;
+		// We create a
+		int position;
+		String mode = "";
+
+		do {
+			System.out.println("a. To show values");
+			System.out.println("b. To introduce a value");
+			System.out.println("c. To exit");
+			mode = keyboard.next();
+			switch (mode) {
+			case "a":
+				System.out.println(Arrays.toString(numbers));
+				break;
+			case "b":
+				System.out.println("Tell me the value you want to introduce: ");
+				value = keyboard.nextInt();
+				System.out.println("Tell me the position of the value: ");
+				position = keyboard.nextInt();
+				numbers[position] = value;
+				break;
+			case "c":
+
+			}
+		} while (!(mode.equalsIgnoreCase("c")));
+
+	}
+
+}
---
+++ Tema3/src/english/Ejercise2.java
@@ -0,0 +1,9 @@
+package english;
+
+public class Ejercise2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
