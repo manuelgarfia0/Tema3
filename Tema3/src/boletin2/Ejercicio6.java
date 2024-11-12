5f4156997cf68fe0c58247cd7c64402e51947fe6
Manuel Garfia <manuelgarfiacastro@gmail.com>
2024-11-12 19:02:56 +0100

    boletín 2 completo

--- Tema3/src/boletin2/Ejercicio6.java
+++ Tema3/src/boletin2/Ejercicio6.java
@@ -6,19 +6,22 @@ import java.util.Random;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		//
+		// Creamos un random
+		Random random = new Random();
+		// Creamos dos tablas, una para declarar la combinación ganadora y otro para los
+		// números aleatorios
 		int primitiva[] = new int[6];
 		int combinacionGanadora[] = { 2, 9, 11, 15, 24, 32, 40 };
-
-		Random random = new Random();
-
+		// Creamos las siguientes variables
 		int numeroAleatorio;
 		int busqueda;
 		int contador = 0;
 		int numeroAciertos = 0;
-
+		// Creo un bucle while para que el contador esté debajo de 6
 		while (contador < 6) {
+			// Hago que salgan números aleatorios
 			numeroAleatorio = random.nextInt(1, 50);
+			// Ordeno la tabla que se va a generar aleatoriamente
 			Arrays.sort(primitiva);
 			busqueda = Arrays.binarySearch(primitiva, numeroAleatorio);
 			if (busqueda < 0) {
@@ -27,14 +30,19 @@ public class Ejercicio6 {
 			}
 
 		}
+		// Muestro en pantalla los valores aleatorios
 		System.out.println(Arrays.toString(primitiva));
 
 		for (int valor : primitiva) {
 			busqueda = Arrays.binarySearch(combinacionGanadora, valor);
+			// Con un if hago que si algún número coincide con la combinación ganadora se
+			// sume al contador
 			if (busqueda >= 0) {
 				numeroAciertos++;
 			}
 		}
+		// Muestro el número de acierto que son los números que coinciden con la
+		// combinación ganadora
 		System.out.println("Número de aciertos: " + numeroAciertos);
 	}
 
--- Tema3/src/boletin2/Ejercicio7.java
+++ Tema3/src/boletin2/Ejercicio7.java
@@ -1,9 +1,22 @@
 package boletin2;
 
+import java.util.Arrays;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		
-	}
+		// Creo una tabla con 55 de tamaño porque es el necesario en este caso
+		int[] numeros = new int[55];
+		// Creo una variable
+		int rep = 0;
 
+		// Creo un for para llenar la tabla con cada número del 1 al 10
+		for (int i = 1; i <= 10; i++) {
+			Arrays.fill(numeros, rep, rep + i, i);
+			rep += i;
+		}
+
+		// Muestro la tabla
+		System.out.println(Arrays.toString(numeros));
+	}
 }
