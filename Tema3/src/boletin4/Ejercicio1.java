9ea2adaf800884287f92460a2ee95f0f5881e563
manuelgarfia0 <manuel.garfia@iesnervion.es>
2024-11-15 14:36:22 +0100

    aodfm

---
+++ Tema3/src/boletin4/Ejercicio1.java
@@ -0,0 +1,16 @@
+package boletin4;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		int tabla[] = new int[5];
+		
+		
+	}
+	static int suma(int tabla[]) {
+		return 0;
+	}
+	
+
+}
