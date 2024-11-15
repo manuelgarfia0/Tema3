50773689c64a87a68a7a84f34239541987448a88
manuelgarfia0 <manuel.garfia@iesnervion.es>
2024-11-15 14:41:56 +0100

    aaa

--- Tema3/src/boletin4/Ejercicio1.java
+++ Tema3/src/boletin4/Ejercicio1.java
@@ -3,14 +3,13 @@ package boletin4;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
+
 		int tabla[] = new int[6];
-		
-		
+
 	}
+
 	static int suma(int tabla[]) {
 		return 0;
 	}
-	
 
 }
