46039370e6cc943f6543a3165d5896a88099f200
Manuel Garfia <manuelgarfiacastro@gmail.com>
2024-11-19 19:04:53 +0100

    Hasta ejercicio 5 boletín 4

--- Tema3/src/boletin4/Ejercicio3.java
+++ Tema3/src/boletin4/Ejercicio3.java
@@ -1,26 +1,40 @@
 package boletin4;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
+		// Ejemplo de prueba
+		int longitud = 5;
+		int fin = 20;
 
-		Random random = new Random();
+		int[] pares = rellenaPares(longitud, fin);
 
-		int numRandom = random.nextInt((50) * 2);
-		int longitud;
-		int fin;
-
+		System.out.println("Arreglo de pares aleatorios:");
+		for (int num : pares) {
+			System.out.println(num);
+		}
 	}
 
 	static int[] rellenaPares(int longitud, int fin) {
+		int[] tabla; // Declaramos el arreglo fuera del flujo condicional
 
-		int[] paresAleatorios = new int[numRandom];
-		Arrays.sort(paresAleatorios);
+		if (longitud <= 0 || fin < 2) {
+			tabla = new int[0]; // Si los parámetros son inválidos, asignamos un arreglo vacío
+		} else {
+			tabla = new int[longitud];
+			Random random = new Random();
 
-		return paresAleatorios;
+			for (int i = 0; i < longitud; i++) {
+				int par;
+				do {
+					par = random.nextInt(fin - 1) + 2; // Genera un número entre 2 y fin inclusive
+				} while (par % 2 != 0); // Asegura que sea par
+				tabla[i] = par;
+			}
+		}
+
+		return tabla;
 	}
-
 }
--- Tema3/src/boletin4/Ejercicio5.java
+++ Tema3/src/boletin4/Ejercicio5.java
@@ -3,16 +3,32 @@ package boletin4;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		int cont = 0;
 		
-		for(i) {
-			
+		int[] t = { 1, 2, 3, 2, 4, 2, 5 };
+		int valor = 2;
+
+		int[] indices = buscarTodos(t, valor);
+
+		System.out.println("Índices donde se encuentra el valor " + valor + ":");
+		for (int indice : indices) {
+			System.out.println(indice);
 		}
 	}
 
 	static int[] buscarTodos(int t[], int valor) {
-		int tamaño = numVeces(t, valor);
+		int tamaño = numVeces(t, valor); // Cantidad de veces que aparece el valor
 		int[] tabla = new int[tamaño];
+
+		if (tamaño == 0) {
+			return tabla; // Devuelve una tabla vacía si no se encuentra el valor
+		}
+
+		int posicion = 0; // Índice para llenar el arreglo `tabla`
+		for (int i = 0; i < t.length; i++) {
+			if (t[i] == valor) {
+				tabla[posicion++] = i;
+			}
+		}
 
 		return tabla;
 	}
