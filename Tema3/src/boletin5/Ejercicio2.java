4c0ba314c6e9f6aae02ff11a6e0eeb78cbda4ed8
Manuel Garfia <manuelgarfiacastro@gmail.com>
2024-11-28 19:45:21 +0100

    Ejercicio 2 Boletín 5

--- Tema3/src/boletin5/Ejercicio2.java
+++ Tema3/src/boletin5/Ejercicio2.java
@@ -1,9 +1,34 @@
 package boletin5;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		
+
+		int tabla[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		desordenarTabla(tabla);
+
+		System.out.println(Arrays.toString(tabla));
+
+	}
+
+	static void desordenarTabla(int[] tabla) {
+
+		Random random = new Random();
+
+		int posInicial;
+		int aux;
+
+		for (int i = 0; i < tabla.length; i++) {
+			posInicial = random.nextInt(0, tabla.length);
+			aux = tabla[i];
+			tabla[i] = tabla[posInicial];
+			tabla[posInicial] = aux;
+		}
+
 	}
 
 }
---
+++ Tema3/src/boletin5/Ejercicio3.java
@@ -0,0 +1,11 @@
+package boletin5;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
