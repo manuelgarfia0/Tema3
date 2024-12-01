8b8c1026392981c29fd4c3cb5b038bc459e7b60f
Manuel Garfia <manuelgarfiacastro@gmail.com>
2024-12-01 15:58:33 +0100

    empezar ejercicio 3

--- Tema3/src/boletin5/Ejercicio3.java
+++ Tema3/src/boletin5/Ejercicio3.java
@@ -1,11 +1,34 @@
 package boletin5;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		int tabla[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+
+		desordenarTabla(tabla);
+
+		System.out.println(Arrays.toString(tabla));
+
+	}
+
+	static void desordenarTabla(int[][] tabla) {
+
+		Random random = new Random();
+
+		int posInicial;
+		int aux;
+
+		for (int i = 0; i < tabla.length; i++) {
+			posInicial = random.nextInt(0, tabla.length);
+			aux = tabla[i];
+			tabla[i] = tabla[posInicial];
+			tabla[posInicial] = aux;
+		}
+
 	}
 
 }
