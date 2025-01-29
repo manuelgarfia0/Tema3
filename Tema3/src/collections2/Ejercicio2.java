edd12b7a5a254c904f379f14d224e06c0807a0d3
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-01-29 09:41:04 +0100

    ejercicio 2

--- Tema3/src/collections2/Ejercicio2.java
+++ Tema3/src/collections2/Ejercicio2.java
@@ -8,14 +8,16 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 
 		Random random = new Random();
-
+		// Lista de números enteros que ordena por insercción
 		LinkedHashSet<Integer> numeros = new LinkedHashSet<Integer>();
+		// Variable para insertar los números en la lista
 		int numero;
-
+		// Introducimos 10 números aleatorios entre 1 y 20
 		while (numeros.size() < 10) {
 			numero = random.nextInt(1, 21);
 			numeros.add(numero);
 		}
+		// Muestro la lista
 		System.out.println(numeros);
 	}
 
